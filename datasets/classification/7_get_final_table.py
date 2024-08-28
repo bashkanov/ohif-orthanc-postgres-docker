a8275cf0671d88d7bc1a87b1b20badcc18456b99
Oleksii Bashkanov <bashkanov.ad@gmail.com>
2024-08-28 23:27:45 +0200

    Updated the dataset with a real annotated data

--- datasets/classification/7_get_final_table.py
+++ datasets/classification/7_get_final_table.py
@@ -2,26 +2,29 @@
 import os
 import pandas as pd
 
-volumes = pd.read_csv("/home/oleksii/projects/ohif-orthanc-postgres-docker/datasets/2_classification/prostate_class_dataset_volumetry.csv")
+volumes = pd.read_csv("/home/oleksii/projects/ohif-orthanc-postgres-docker/datasets/classification/prostate_class_dataset_volumetry.csv")
+volumes_manual = pd.read_csv("/home/oleksii/projects/ohif-orthanc-postgres-docker/datasets/classification/prostate_class_dataset_volumetry_zonal_dataset.csv", sep=';')
+merged_volumes = pd.merge(volumes, volumes_manual, on='study_oid', how='left', suffixes=('', '_manual'))
+merged_volumes['volume_pz'] = merged_volumes['volume_pz_manual']
+merged_volumes['volume_tz'] = merged_volumes['volume_tz_manual']
 
-volumes.rename(columns={'study_oid': 'study_orthanc_id'}, inplace=True)
-volumes = volumes[['study_orthanc_id', 'volume_pz', 'volume_tz']]
+merged_volumes.rename(columns={'study_oid': 'study_orthanc_id'}, inplace=True)
+merged_volumes = merged_volumes[['study_orthanc_id', 'volume_pz', 'volume_tz']]
 
-df = pd.read_csv("/home/oleksii/projects/ohif-orthanc-postgres-docker/datasets/2_classification/prostate_class_dataset_demography_final_psa.csv", sep=";")
+
+df = pd.read_csv("/home/oleksii/projects/ohif-orthanc-postgres-docker/datasets/classification/prostate_class_dataset_demography_final_psa.csv", sep=";")
 
 final_dataset = pd.merge(
     df,
-    volumes,
+    merged_volumes,
     how="left",
     on='study_orthanc_id',
 )
 
-data_path_preprocess = "/data/oleksii/Prostate-Classification-Datasets-NRRDS/ALTA-Classification-Dataset-preprocess/"
+data_path_preprocess = "/data/oleksii/Prostate-ZONE-Datasets-NRRDS/ALTA-Zone-Dataset-multimodal-preprocessed"
 
 preprocessed_studies = os.listdir(path=data_path_preprocess)
 
 final_dataset = final_dataset[final_dataset['study_orthanc_id'].isin(preprocessed_studies)]
 final_dataset = final_dataset[~final_dataset['volume_pz'].isna()]
-
-
-final_dataset.to_csv("/home/oleksii/projects/ohif-orthanc-postgres-docker/datasets/2_classification/prostate_class_dataset_demography_final_psa_vol.csv", sep=";", index=False)
+final_dataset.to_csv("/home/oleksii/projects/ohif-orthanc-postgres-docker/datasets/classification/prostate_class_dataset_demography_final_psa_vol.csv", sep=";", index=False)
